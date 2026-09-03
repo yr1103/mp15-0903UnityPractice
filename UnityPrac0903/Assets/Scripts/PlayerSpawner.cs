472c0cc816bee8a21690cca438786dc725ea1aab
youngrae <youngrae1103@gmail.com>
2026-09-03 10:45:15 +0900

    [Feat] PlayerSpawner 코드 수정

--- UnityPrac0903/Assets/Scripts/PlayerSpawner.cs
+++ UnityPrac0903/Assets/Scripts/PlayerSpawner.cs
@@ -18,9 +18,8 @@ public class PlayerSpawner : MonoBehaviour
         }
 
         //파괴
-
-        if(Input.GetKeyDown(KeyCode.D))
-        
+        //D키가 눌렸으면서 List의 크기가 0이 아닐때 - prefab하나라도 있을 때만 파괴
+        if(Input.GetKeyDown(KeyCode.D) && _players.Count != 0)
         {
             // 리스트에서 잘 빠지고 있는지 확인해야한다
             // 이 두 줄이 중요하다.
